d421279bb7f00c2f487c40bb397ba716685bd825
riannaliu <rianna.liu@tufts.edu>
2024-12-10 19:29:07 -0500

    Update app2.js

--- app2.js
+++ app2.js
@@ -1,8 +1,6 @@
 const express = require("express");
-require('dotenv').config();
 const { MongoClient } = require("mongodb");
 const fs = require("fs");
-require("dotenv").config(); // Load .env file for MongoDB URI
 
 const app = express();
 const PORT = process.env.PORT || 3000;
